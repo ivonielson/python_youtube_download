e55b86beefa82f284c88fe1414f87d7f7fb6234b
Ivonielson <ivonielsonfreitas@gmail.com>
2026-04-15 18:58:44 -0400

    Ignorar pasta Downloads

--- app.py
+++ app.py
@@ -741,4 +741,4 @@ def index():
 
 if __name__ == '__main__':
     cleanup_old_files(24)
-    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
+    app.run(debug=False, host='0.0.0.0', port=5000, threaded=True)
